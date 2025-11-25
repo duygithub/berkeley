a06f1d9e3dd10a8d37f23affdd86df715eb11ef9
Duy Bao Vo <duyvo@berkeley.edu>
2025-11-25 20:29:46 +0000

    Incremental work

--- datasci203/unit09_programming/Q3.3.R
+++ datasci203/unit09_programming/Q3.3.R
@@ -2,22 +2,21 @@ library(broom)
 library(dplyr)
 library(ggplot2)
 
-rmystery <- function(n){
+renigma <- function(n){
   x = runif(n)
-  y = runif(n, min=0, max = 1/x)
+  y = runif(n, min=0, max = (1-x)^2)
   data.frame(x=x,y=y)
 }
-plot(rmystery(100))
-
+plot(renigma(100))
 
 experiment_m <- function(number_of_samples) {
-  data_frame = rmystery(100)
-  sample_df = sample_n(data_frame, number_of_samples)
+  data_frame <- renigma(100)
+  sample_df <- sample_n(data_frame, number_of_samples)
 }
 
 calculate_slope <- function(data_frame) {
-  model <- lm(y ~x, data_frame)
-  regression_coefficients <- coef(model)
+  model <- lm(y ~ x, data_frame)
+  regression_coefficients = coef(model)
   slope <- regression_coefficients[2]
 }
 
@@ -29,7 +28,9 @@ for (i in 1:10) {
 }
 
 
-slope_samples <- replicate(1000, calculate_slope(sample_n(experiment_m(100), 100)))
+slope_samples <- replicate(100, calculate_slope(sample_n(experiment_m(100), 100)))
+
+slope_samples
 
 min_slope <- min(slope_samples)
 max_slope <- max(slope_samples)
@@ -38,5 +39,9 @@ print(paste("Minimum Slope:", min_slope))
 print(paste("Maximum Slope:", max_slope))
 
 hist(slope_samples,
-     breaks = 100,
-     xlim = c(-200, 0))
+     breaks = 40,
+     xlim = c(-.9, 0))
+
+
+
+
